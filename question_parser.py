77b43674575fba13f1a7edff7d194133e0a9bab7
unknown <chenyx735@chinaunicom.cn>
2023-03-21 15:23:18 +0800

    question_parser.py

---
+++ question_parser.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# @Author : Eason_Chen
+
+# @Time : 2023/3/21 下午 03:21
+
+class QuestionPaser:
+    '''构建实体节点'''
+    def build_entitydict(self, args):
+        return ""
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    handler = QuestionPaser()
